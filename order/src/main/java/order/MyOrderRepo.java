f1033e9e526e80fccbec3aec388c00e524eb40b0
Vadim <osipov.13.vadim@gmail.com>
2019-10-10 17:33:55 +0300

    Remove old order-service; added contributing

--- order/src/main/java/order/MyOrderRepo.java
+++
@@ -1,7 +0,0 @@
-package order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MyOrderRepo extends JpaRepository<Order, Integer> {
-}
