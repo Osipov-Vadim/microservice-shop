b39763fc3d538547df094e7ba6b8d7c72f5393a5
AnneKsatn <AnneKsatn@users.noreply.github.com>
2019-11-04 14:59:57 +0300

    Order + payment images

--- order-service/settings.py
+++ order-service/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db_order_service.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_order_service.sqlite3'),
     }
 }
 
